d6b448699c21bea2aa03f61890ce4beb644610cc
rohithpala <rohithpala02@gmail.com>
2022-02-12 18:05:42 +0530

    Portfolio

---
+++ common/common.js
@@ -0,0 +1,14 @@
+document.querySelector(".common").innerHTML = `
+   <div>
+      <a href="portfolio.html"><img class="logo" src="../images/portfolio.png" alt="Logo"></a>
+   </div>
+
+   <span></span>
+
+   <ul class="menu">
+      <li class="menu-item"><a href="portfolio.html">Home</a></li>
+      <li class="menu-item"><a href="works.html">My Works</a></li>
+      <li class="menu-item"><a href="resume.html">Resume</a></li>
+      <li class="menu-item"><a href="contact.html">Contact Me</a></li>
+   </ul>
+`;
