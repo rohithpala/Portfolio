32b6f921f49b643e1cdab73e4d66bbb732888f90
rohithpala <rohithpala02@gmail.com>
2022-02-13 22:46:06 +0530

    portfolio - 3/4 done

--- common/common.js
+++ common/common.js
@@ -11,3 +11,23 @@ document.querySelector(".common").innerHTML = `
       <li class="menu-item"><a href="resume.html">Resume</a></li>
    </ul>
 `;
+
+document.querySelector(".small-menu").innerHTML = `
+   <div class="dots">
+      <i class="fa-solid fa-ellipsis menu-icon" onclick="showMenu()"></i>
+   </div>
+
+   <div class="menu1">
+      <ul>
+         <li><a href="#">Portfolio</a></li>
+         <li><a href="#">My Works</a></li>
+         <li><a href="#">Resume</a></li>
+      </ul>
+   </div>
+`;
+
+const menu = document.querySelector(".menu");
+function showMenu() {
+   if (menu.style.maxHeight) menu.style.maxHeight = null;
+   else menu.style.maxHeight = menu.scrollHeight + "px";
+}
